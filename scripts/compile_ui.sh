1c150c812b88617fa98921b05641e8283adee599
Lorenzo Castellino <castellino96@gmail.com>
2022-09-30 11:58:32 +0200

    [feat] Add profiling and benchmarks utilities

--- scripts/compile_ui.sh
+++ scripts/compile_ui.sh
@@ -1,8 +1,7 @@
-#!/bin/zsh
-poetry shell
+#!/bin/sh
 for filename in src/main/python/ui/*.ui; do
-    pyside6-uic $filename -o ${filename%%.*}.py --star-imports
+    poetry run pyside6-uic $filename -o ${filename%%.*}.py --star-imports
 done
-pyside6-rcc src/main/resources/base/resources.qrc -o src/main/python/resources_rc.py
+poetry run pyside6-rcc src/main/resources/base/resources.qrc -o src/main/python/resources_rc.py
 echo "Qt files compiled!"
 # pipenv run python iconCreator.py
---
+++ scripts/profile.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+
+poetry run pyinstrument -o benchmark/profile.html benchmark/test.py mockup/urine_silvia_modelA_solid_var_error.json
+xdg-open benchmark/profile.html > /dev/null
