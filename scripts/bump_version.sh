03bd378c012d67704e963a0445833b2201212ad3
Lorenzo Castellino <castellino96@gmail.com>
2022-10-12 16:07:36 +0200

    [chore] Script for version bumping/release notes

---
+++ scripts/bump_version.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# display all the arguments using for loop
+if [ $# -ne 2 ]
+then
+
+  echo "Call the script with current and target vx.x.x version"
+
+else
+  tbump $2 --dry-run &&
+  poetry run python scripts/changelog_generator.py v$1 v$2
+
+fi
