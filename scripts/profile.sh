a9c186b1b6a1ca8cfe303500f7dc69e37588c71d
Lorenzo Castellino <castellino96@gmail.com>
2023-12-05 11:34:27 +0100

    [chore] Improve reliability of benchmark scripts

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 
-poetry run pyinstrument -o benchmark/profile.html benchmark/test.py mockup/urine_silvia_modelA_solid_var_error.json
+PYTHONPATH="." poetry run pyinstrument -o benchmark/profile.html benchmark/test.py mockup/urine_silvia_modelA_solid_var_error.json
 xdg-open benchmark/profile.html > /dev/null
--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -2,11 +2,11 @@
 mkdir -p benchmark/benchmark_results
 mkdir -p benchmark/profiles
 
-hyperfine -r 5 --export-json "benchmark/benchmark_results/results.json"\
+hyperfine --show-output -r 5  --export-json "benchmark/benchmark_results/results.json"\
             --setup "source /home/lorenzo/.cache/pypoetry/virtualenvs/pyes-YsROe161-py3.10/bin/activate"\
-            --parameter-list file_name ac,phoenix_var,urine_silvia_modelA_solid_var,urine_silvia_modelA_solid_var_error\
-            "python benchmark/test.py mockup/{file_name}.json"\
-            --cleanup "poetry run pyinstrument -o benchmark/profiles/profile_{file_name}.html benchmark/test.py mockup/{file_name}.json & (deactivate  || true)"
+            --parameter-list file_name ac,phoenix_var,urine_silvia_modelA_solid_var,urine_silvia_modelA_solid_var_error,artici\
+            "PYTHONPATH="." python benchmark/test.py mockup/{file_name}.json"\
+            --cleanup "PYTHONPATH="." poetry run pyinstrument -o benchmark/profiles/profile_{file_name}.html benchmark/test.py mockup/{file_name}.json & (deactivate  || true)"
 
 poetry run python benchmark/benchmark_viz/advanced_statistics.py benchmark/benchmark_results/results.json > benchmark/benchmark_results/advanced_statistics.log
 poetry run python benchmark/benchmark_viz/plot_whisker.py benchmark/benchmark_results/results.json --output benchmark/benchmark_results/whisker_plt_results.png
